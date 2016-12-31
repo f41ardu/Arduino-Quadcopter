73c3b7d589aba8b0559fae2fa1dfd214b7432a49
f41ardu <f41thr@arcor.de>
2016-12-31 06:51:31 +0100

    Add Chabgelog Ino
    replace IMU.ino by MPU6050.ino, remove libraries folder and use
    libraries from. Arduino base installation. Arduino IDE is 1.6.13

---
+++ Quadcopter/Changelog.ino
@@ -0,0 +1,5 @@
+/* 
+ *  Changelog 
+ *  2016-21-31 f41ardu - replace IMU.ino by MPU6050.ino, remove libraries folder and use libraries from  
+ *  Arduino base installation. Arduino IDE is 1.6.13 
+ */
