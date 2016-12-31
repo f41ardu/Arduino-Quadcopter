c9f982aeb0390cf412a7875d782c6e2c122d1ab0
f41ardu <f41thr@arcor.de>
2016-12-31 07:53:55 +0100

    YAWPITCHROLL calculation updated
    Use YAWPITCHROLL calculation from MPU6050_6Axis_MotionApps20.h library

--- Quadcopter/Changelog.ino
+++ Quadcopter/Changelog.ino
@@ -2,4 +2,6 @@
  *  Changelog 
  *  2016-21-31 f41ardu - replace IMU.ino by MPU6050.ino, remove libraries folder and use libraries from  
  *  Arduino base installation. Arduino IDE is 1.6.13 
+ *  
+ *  Use YAWPITCHROLL calculation from MPU6050_6Axis_MotionApps20.h library
  */
